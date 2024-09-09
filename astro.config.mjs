1fe8d1cf3c9896c581fe5d4a0648a1e9c71dab0e
Alex Bosco <ajbosco00@gmail.com>
2024-09-09 13:54:25 -0400

    test

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,12 +2,12 @@ import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
 
-import netlify from "@astrojs/netlify";
+// import netlify from "@astrojs/netlify";
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://example.com',
   integrations: [mdx(), sitemap()],
-  output: 'hybrid',
-  adapter: netlify()
+  output: 'static',
+//   adapter: netlify()
 });
